e3153e5ebf6ef9254b292f6f40119bd95ac49f1a
wndyylnda <windyyolanda0052gmail.com>
2025-04-23 12:48:47 +0700

    selesai

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', [UserController::class, 'create'])->name('user.create');
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
