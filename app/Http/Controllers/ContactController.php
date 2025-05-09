05565a4229c36692de8c389d3c9d66cb070192f2
wndyylnda <windyyolanda0052gmail.com>
2025-05-09 14:22:11 +0700

    Admin Commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('contact.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'    => 'required|string|max:100',
+            'email'   => 'required|email',
+            'phone'   => 'required|digits_between:10,15',
+            'message' => 'required|string|max:500',
+        ]);
+
+        // Proses bisa disimpan ke database atau dikirim via email, tapi ini hanya redirect
+        return redirect()->back()->with('success', 'Pesan Anda berhasil dikirim!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', [UserController::class, 'create'])->name('user.create');
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
 
 Route::get('/contact/create', [ContactController::class, 'create'])->name('contact.create');
-Route::post('/contact/create', [ContactController::class, 'store'])->name('contact.store');
+Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
+Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
